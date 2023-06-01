414810cdc0b0164e1d5115a13d48a8267f55ae54
panappi <119737780+panappi@users.noreply.github.com>
2023-06-01 15:45:39 +0900

    style: ハンバーガーメニューページのデザイン実装、共通デザイン修正、変数切り出し

--- js/hamburgermenu.js
+++ js/hamburgermenu.js
@@ -1,22 +1,22 @@
-// 3本線から✗に変化するボタン-1
+// 3本線から✕に変化するボタン-1
 const btn_3x = document.getElementById("btn_3x");
 btn_3x.addEventListener("click", function () {
   this.classList.toggle("active");
 });
 
-// 3本線から✗に変化するボタン-2
+// 3本線から✕に変化するボタン-2
 const btn_3x_2 = document.getElementById("btn_3x_2");
 btn_3x_2.addEventListener("click", function () {
   this.classList.toggle("active");
 });
 
-// 2本線から✗に変化するボタン
+// 2本線から✕に変化するボタン
 const btn_2x = document.getElementById("btn_2x");
 btn_2x.addEventListener("click", function () {
   this.classList.toggle("active");
 });
 
-// MENUから✗に変化するボタン
+// MENUから✕に変化するボタン
 const btn_mx = document.getElementById("btn_mx");
 btn_mx.addEventListener("click", function () {
   this.classList.toggle("active");
