befd5aa9701e71119d111bbc6b069bf65dfb6e18
panappi <119737780+panappi@users.noreply.github.com>
2023-06-14 07:00:50 +0900

    feat: hamburgermenuにメニューリストの動きを確認する要素を追加

--- js/common.js
+++ js/common.js
@@ -1,8 +1,37 @@
-const nav_sp = document.getElementById("nav_sp");
 const nav_btn = document.getElementById("nav_btn");
+const nav_tb = document.getElementById("nav_tb");
+const nav_sp = document.getElementById("nav_sp");
+const breakpointTb = window.matchMedia("(min-width: 744px)"); // 744px以上
+
 const toggleActive = (e) => {
   e.classList.toggle("active");
 };
+const toggleHide = (e) => {
+  e.classList.toggle("hide");
+};
+const removeActive = (e) => {
+  e.classList.remove("active");
+};
+const toggleNav = (e) => {
+  if (e.matches) {
+    toggleHide(nav_tb),
+      toggleHide(nav_btn),
+      removeActive(nav_sp),
+      removeActive(nav_btn);
+  } else {
+    toggleHide(nav_tb), toggleHide(nav_btn);
+  }
+};
+
+// 画面サイズに合わせてナビメニューの表示を切り替える
+// ページを読み込んだときに実行(744px以上の場合sp用からtb用に切り替える)
+if (breakpointTb.matches) {
+  toggleHide(nav_tb), toggleHide(nav_btn);
+}
+// ウィンドウサイズを変更したときに実行（ブレイクポイントを監視する）
+breakpointTb.addEventListener("change", toggleNav);
+
+// sp用ナビメニューリストの表示・非表示、ボタンのスタイルを切り替える
 nav_btn.addEventListener("click", () => {
   toggleActive(nav_sp), toggleActive(nav_btn);
 });
--- js/hamburgermenu.js
+++ js/hamburgermenu.js
@@ -1,6 +1,9 @@
-const toggleActive = (e) => {
-  e.classList.toggle("active");
-};
+// common.jsから読み込み
+// const toggleActive = (e) => {
+//   e.classList.toggle("active");
+// };
+
+// contents
 // 3本線から✕に変化するボタン-1
 const btn_3x = document.getElementById("btn_3x");
 btn_3x.addEventListener("click", () => {
@@ -13,6 +16,12 @@ btn_3x_2.addEventListener("click", () => {
   toggleActive(btn_3x_2);
 });
 
+// 3本線から✕に変化するボタン-3
+const btn_3x_3 = document.getElementById("btn_3x_3");
+btn_3x_3.addEventListener("click", () => {
+  toggleActive(btn_3x_3);
+});
+
 // 2本線から✕に変化するボタン
 const btn_2x = document.getElementById("btn_2x");
 btn_2x.addEventListener("click", () => {
@@ -23,4 +32,11 @@ btn_2x.addEventListener("click", () => {
 const btn_mx = document.getElementById("btn_mx");
 btn_mx.addEventListener("click", () => {
   toggleActive(btn_mx);
+});
+
+// mockup
+const mockup_btn = document.getElementById("mockup_btn");
+const mockup_nav = document.getElementById("mockup_nav");
+mockup_btn.addEventListener("click", () => {
+  toggleActive(mockup_btn), toggleActive(mockup_nav);
 });
